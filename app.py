6bb61b15000933539e9da80f704c03c4405c3c8d
Gal Moore <gal@galmoore.co.uk>
2023-07-11 13:48:05 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import os
+import PyPDF2
+from io import BytesIO
+import openai
+import time
+import os
